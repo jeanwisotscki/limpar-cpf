be331761656192067383c1d18ff747077d3fea2c
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-02-13 11:31:46 -0300

    correções no html e script finalizado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-const cpfs = document.querySelectorAll(".cpf li");
+const listaCPFS = document.querySelectorAll(".cpf li");
+const btnFormatar = document.querySelector("#btn-formatar");
 
 const elementsInnerText = ([...elements]) => {
   return elements.map((element) => element.innerText);
 };
 
-console.log(elementsInnerText(cpfs));
+const limparCPF = (cpf) => cpf.replace(/\D/g, "");
+
+const construirCPF = (cpf) => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
+};
+
+const formatarCPFS = (cpfs) => {
+  return cpfs.map(limparCPF).map(construirCPF);
+};
+
+const substituiCPFS = (cpfsElements) => {
+  const cpfs = elementsInnerText(cpfsElements);
+  const cpfsFormatados = formatarCPFS(cpfs);
+
+  btnFormatar.addEventListener("click", () => {
+    cpfsElements.forEach((element, index) => {
+      element.innerText = cpfsFormatados[index];
+    });
+  });
+};
+
+substituiCPFS(listaCPFS);
